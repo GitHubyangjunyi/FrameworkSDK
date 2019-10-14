4fe999fbadbda7165c4cc69608c058273cf5fa92
杨俊艺 <1209032479@qq.com>
2019-10-14 19:39:13 +0800

    sync

--- FrameworkSDK/FrameworkSDK.h
+++ FrameworkSDK/FrameworkSDK.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char FrameworkSDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <FrameworkSDK/PublicHeader.h>
 
-
+#import <FrameworkSDK/MySDK.h>
---
+++ FrameworkSDK/MySDK.m
@@ -0,0 +1,18 @@
+//
+//  MySDK.m
+//  FrameworkSDK
+//
+//  Created by 杨俊艺 on 2019/10/14.
+//  Copyright © 2019 杨俊艺. All rights reserved.
+//
+
+#import "MySDK.h"
+
+@implementation MySDK
+
+-(void)demoOutput
+{
+    NSLog(@"Framework output");
+}
+
+@end
